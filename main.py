039008e877b00b947458bfe3b9a7cb6de3de04c5
Nathan Bradshaw <109757621+nathan-bradshaw@users.noreply.github.com>
2024-04-01 22:09:52 -0500

    simple change

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 """
 Created on Thu Oct  8 23:29:10 2020
 
-@author: mehedi.md.hasan
+@author: nathan-bradshaw
 """
 from util import Util
 from iac_testing_antipatterns import IaCTestingAntipatterns
